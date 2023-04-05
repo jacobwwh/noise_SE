e52265319a8c8266f9f00551e62032dec4446659
Wang Wenhan <wwhjacob@hotmail.com>
2023-04-05 15:23:56 +0800

    Update codenet_utils.py

--- utils/codenet_utils.py
+++ utils/codenet_utils.py
@@ -21,7 +21,7 @@ def random_noise_label(num_classes,correct_label):
     return label
 
 
-def read_codenetdata(dataname,mislabeled_rate=0.2,noise_pattern='random'):
+def read_codenetdata(dataname,mislabeled_rate=0.2,noise_pattern='random',noisy_test=False):
     datapath=datapaths[dataname]
     if dataname=='java250':
         num_classes=250
@@ -72,4 +72,39 @@ def read_codenetdata(dataname,mislabeled_rate=0.2,noise_pattern='random'):
                             train_data[i][j]['label']=i+1
                         else:
                             train_data[i][j]['label']=i-1
+        if noisy_test:
+            print('create noisy valid/test set')
+            for i in range(num_classes):
+                mislabeld_dev_idx=random.sample(range(len(dev_data[i])),int(len(dev_data[i])*mislabeled_rate))
+                mislabeld_dev_idx=set(mislabeld_dev_idx)
+                for j in range(len(dev_data[i])):
+                    if j in mislabeld_dev_idx:
+                        if noise_pattern=='random':
+                            dev_data[i][j]['label']=random_noise_label(num_classes,i)
+                        elif noise_pattern=='flip':
+                            dev_data[i][j]['label']=(i+1)%num_classes
+                        elif noise_pattern=='pair':
+                            if i%2==0:
+                                dev_data[i][j]['label']=i+1
+                            else:
+                                dev_data[i][j]['label']=i-1
+                        else:
+                            print('undefined noise pattern!')
+                            quit()
+                mislabeld_test_idx=random.sample(range(len(test_data[i])),int(len(test_data[i])*mislabeled_rate))
+                mislabeld_test_idx=set(mislabeld_test_idx)
+                for j in range(len(test_data[i])):
+                    if j in mislabeld_test_idx:
+                        if noise_pattern=='random':
+                            test_data[i][j]['label']=random_noise_label(num_classes,i)
+                        elif noise_pattern=='flip':
+                            test_data[i][j]['label']=(i+1)%num_classes
+                        elif noise_pattern=='pair':
+                            if i%2==0:
+                                test_data[i][j]['label']=i+1
+                            else:
+                                test_data[i][j]['label']=i-1
+                        else:
+                            print('undefined noise pattern!')
+                            quit()                    
     return train_data,dev_data,test_data
