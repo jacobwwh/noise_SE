aebf67d6df33136bb3f89dd9e253f413c1a331f5
Li Yanzhou <liyanzhou97@outlook.com>
2023-04-26 11:17:21 +0800

    change tracin

--- run_classification_tracin.py
+++ run_classification_tracin.py
@@ -39,7 +39,9 @@ def boolean_string(s):
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--metaset", type=boolean_string, default=False, help="use metaset for reweighting")
-parser.add_argument("--dataset", type=str, default='poj')
+parser.add_argument("--dataset", type=str, default='java250')
+parser.add_argument('--noise_rate', type = float, help = 'corruption rate, should be less than 1', default = 0.2)
+parser.add_argument("--noise_pattern", default="random", type=str, help="Noise pattern(random/flip/pair).")
 parser.add_argument('--meta_net_hidden_size', type=int, default=100)
 parser.add_argument('--meta_net_num_layers', type=int, default=1)
 parser.add_argument('--meta_lr', type=float, default=1e-6)
@@ -66,7 +68,7 @@ parser.add_argument('--gradient_accumulation_steps', type=int, default=1,
 parser.add_argument("--epochs", default=50, type=int, help="Training epochs.")
 parser.add_argument('--fp16', action='store_true',
                     help="Whether to use 16-bit (mixed) precision (through NVIDIA apex) instead of 32-bit")
-parser.add_argument("--ckpt_path", type=str, default='../result')
+parser.add_argument("--ckpt_path", type=str, default='./result')
 args = parser.parse_args()
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -81,6 +83,144 @@ elif args.dataset == 'java250':
 elif args.dataset == 'python800':
     num_classes = 800  # codenet python800
 
+if args.model_type not in ['gcn', 'gin', 'ggnn', 'hgt']:
+    if args.model_type == 'codebert':
+        tokenizer = RobertaTokenizer.from_pretrained("microsoft/codebert-base")
+        encoder_config = RobertaConfig.from_pretrained("microsoft/codebert-base")
+        encoder_config.num_labels = num_classes
+        pre_model_encoder = RobertaForSequenceClassification.from_pretrained("microsoft/codebert-base",
+                                                                         config=encoder_config)
+        # model_encoder = RobertaForSequenceClassification._from_config(encoder_config) #no pre-trained weights
+    elif args.model_type == 'graphcodebert':
+        tokenizer = RobertaTokenizer.from_pretrained("microsoft/graphcodebert-base")
+        encoder_config = RobertaConfig.from_pretrained("microsoft/graphcodebert-base")
+        encoder_config.num_labels = num_classes
+        pre_model_encoder = RobertaForSequenceClassification.from_pretrained("microsoft/graphcodebert-base",
+                                                                         config=encoder_config)
+    elif args.model_type == 'unixcoder':
+        tokenizer = RobertaTokenizer.from_pretrained("microsoft/unixcoder-base")
+        encoder_config = RobertaConfig.from_pretrained("microsoft/unixcoder-base")
+        encoder_config.num_labels = num_classes
+        pre_model_encoder = RobertaForSequenceClassification.from_pretrained("microsoft/unixcoder-base",
+                                                                         config=encoder_config)
+
+    if args.block_size <= 0:
+        args.block_size = tokenizer.max_len_single_sentence  # Our input block size will be the max possible for the model
+        args.block_size = min(args.block_size, tokenizer.max_len_single_sentence)
+
+    pre_model_encoder.to(device)
+
+if args.model_type in ['gcn', 'gin', 'ggnn', 'hgt']:
+    print('use gnn: ', args.model_type)
+    if args.dataset in ['java250', 'python800']:
+        train_samples, valid_samples, test_samples, token_vocabsize, type_vocabsize = get_spt_dataset(data=args.dataset,
+                                                                                                      mislabeled_rate=args.noise_rate, noise_pattern = args.noise_pattern)
+    else:
+        raise NotImplementedError
+    trainset = GraphClassificationDataset(train_samples)
+    validset = GraphClassificationDataset(valid_samples)
+    testset = GraphClassificationDataset(test_samples)
+    print(len(trainset), len(validset), len(testset))
+
+    pre_model = GNN_codenet(256, num_classes, num_layers=5, token_vocabsize=token_vocabsize, type_vocabsize=type_vocabsize,
+                        model=args.model_type).to(device)
+    train_dataloader = GraphDataLoader(trainset, batch_size=1, shuffle=False)
+    valid_dataloader = GraphDataLoader(validset, batch_size=args.batch_size, shuffle=False)
+    test_dataloader = GraphDataLoader(testset, batch_size=1, shuffle=False)
+else:
+    if args.dataset == 'poj':
+        train_samples, valid_samples, test_samples = generate_pojdata(mislabeled_rate=0.2)
+    if args.dataset in ['java250', 'python800']:
+        train_samples, valid_samples, test_samples = read_codenetdata(dataname=args.dataset, mislabeled_rate=args.noise_rate, noise_pattern = args.noise_pattern)
+
+    trainset = ClassificationDataset(tokenizer, args, test_samples)
+    validset = ClassificationDataset(tokenizer, args, valid_samples)
+    testset = ClassificationDataset(tokenizer, args, test_samples)
+    print('len train sample ', len(train_samples))
+    print('len test sample ', len(test_samples))
+    print('len train samples ', len(trainset.examples))
+    print('len testset ', len(testset.examples))
+
+    # train_subset = torch.utils.data.Subset(trainset, range(50))
+    # test_subset = torch.utils.data.Subset(testset, range(20))
+    # print(len(trainset), len(validset), len(testset))
+
+    # choose classifier: pre-trained or lstm
+    pre_model = bert_classifier_self(pre_model_encoder, encoder_config, tokenizer, args)
+    # model=lstm_classifier(encoder_config.vocab_size,128,128,num_classes)
+    pre_model = pre_model.to(device)
+    train_dataloader = DataLoader(trainset, shuffle=False, batch_size=1, num_workers=0)
+    valid_dataloader = DataLoader(validset, shuffle=False, batch_size=args.batch_size, num_workers=0)
+    test_dataloader = DataLoader(testset, shuffle=False, batch_size=1, num_workers=0)
+
+ckpt_path = os.path.join(args.ckpt_path, args.model_type)
+paths  = os.listdir(ckpt_path)
+for i,path in enumerate(paths):
+    paths[i] = os.path.join(os.path.join(os.path.abspath(ckpt_path), path), 'model.bin')
+score_matrix, idxs = calculate_tracin_score(
+        args=args,
+        model = pre_model,
+        weights_paths = paths,
+        train_dataloader = train_dataloader,
+        test_dataloader = test_dataloader,
+        nu_for_each_epoch = 1e-5)
+# exist = (score_matrix != 0)
+# num = score_matrix.sum(axis = 1)
+# den = exist.sum(axis = 1) + 0.01
+# avg = num / den
+
+st = np.argsort(score_matrix)
+indexes = st[int(st.shape[0] * (1-args.noise_rate)) :]
+#print('indexes len', len(indexes))
+another_indexes = st[:int(st.shape[0] * args.noise_rate)]
+noisy_index = [idxs[x] for x in indexes]
+# print('originial index is ', idxs)
+# print('indexes is ', indexes )
+# print('noisy_index is ', noisy_index)
+another_index = [idxs[x] for x in another_indexes]
+fp = 0
+tp = 0
+for nindex in noisy_index:
+    label = trainset.examples[nindex].label
+    ori_label = trainset.examples[nindex].original_label
+    if label == ori_label:
+        fp += 1
+    else:
+        tp += 1
+precision = float(tp / (fp + tp))
+recall = float(tp / (len(trainset.examples) * args.noise_rate))
+print('precision ', precision)
+print('recall ', recall)
+prefix = './' + str(precision) + '_' + str(recall) + '_' + args.model_type + '_' + str(args.noise_rate) + '_' + args.noise_pattern + '.txt'
+with open(prefix, 'w') as f:
+    f.write(prefix)
+
+inequal = 0
+tt = 0
+true_indexes = []
+for index in st:
+    label = trainset.examples[index].label
+    ori_label = trainset.examples[index].original_label
+    tt += 1
+    if label != ori_label:
+        inequal += 1
+        true_indexes.append(index)
+print('noise rate inequa: ', inequal, ' /  total: ', tt)
+# print('ground truth index is ', true_indexes)
+
+fp = 0
+tp = 0
+for nindex in another_index:
+    label = trainset.examples[nindex].label
+    ori_label = trainset.examples[nindex].original_label
+    if label == ori_label:
+        fp += 1
+    else:
+        tp += 1
+aprecision = float(tp / (fp + tp))
+arecall = float(tp / (len(trainset.examples) * args.noise_rate))
+# print('another precision ', aprecision)
+# print('anotherrecall ', arecall)
 if args.model_type not in ['gcn', 'gin', 'ggnn', 'hgt']:
     if args.model_type == 'codebert':
         tokenizer = RobertaTokenizer.from_pretrained("microsoft/codebert-base")
@@ -110,70 +250,11 @@ if args.model_type not in ['gcn', 'gin', 'ggnn', 'hgt']:
 
 if args.model_type in ['gcn', 'gin', 'ggnn', 'hgt']:
     print('use gnn: ', args.model_type)
-    if args.dataset in ['java250', 'python800']:
-        train_samples, valid_samples, test_samples, token_vocabsize, type_vocabsize = get_spt_dataset(data=args.dataset,
-                                                                                                      mislabeled_rate=args.noise_rate)
-    else:
-        raise NotImplementedError
-    trainset = GraphClassificationDataset(train_samples)
-    validset = GraphClassificationDataset(valid_samples)
-    testset = GraphClassificationDataset(test_samples)
-    print(len(trainset), len(validset), len(testset))
-
-    model = GNN_codenet(256, num_classes, num_layers=5, token_vocabsize=token_vocabsize, type_vocabsize=type_vocabsize,
-                        model=args.model_type).to(device)
-    train_dataloader = GraphDataLoader(trainset, batch_size=1, shuffle=False)
-    valid_dataloader = GraphDataLoader(validset, batch_size=args.batch_size, shuffle=False)
-    test_dataloader = GraphDataLoader(testset, batch_size=1, shuffle=False)
-else:
-    if args.dataset == 'poj':
-        train_samples, valid_samples, test_samples = generate_pojdata(mislabeled_rate=0.2)
-    if args.dataset in ['java250', 'python800']:
-        train_samples, valid_samples, test_samples = read_codenetdata(dataname=args.dataset, mislabeled_rate=0.2)
-
-    trainset = ClassificationDataset(tokenizer, args, train_samples)
-    validset = ClassificationDataset(tokenizer, args, valid_samples)
-    testset = ClassificationDataset(tokenizer, args, test_samples)
-    train_subset = torch.utils.data.Subset(trainset, range(50))
-    test_subset = torch.utils.data.Subset(testset, range(20))
-    print(len(trainset), len(validset), len(testset))
-
-    # choose classifier: pre-trained or lstm
-    model = bert_classifier_self(model_encoder, encoder_config, tokenizer, args)
-    # model=lstm_classifier(encoder_config.vocab_size,128,128,num_classes)
-    model = model.to(device)
-
-    train_dataloader = DataLoader(train_subset, shuffle=False, batch_size=1, num_workers=0)
-    valid_dataloader = DataLoader(validset, shuffle=False, batch_size=args.batch_size, num_workers=0)
-    test_dataloader = DataLoader(test_subset, shuffle=False, batch_size=1, num_workers=0)
-
-
-paths  = os.listdir(args.ckpt_path)
-for i,path in enumerate(paths):
-    paths[i] = os.path.join(os.path.join(os.path.abspath(args.ckpt_path), path), 'model.bin')
-score_matrix, idxs = calculate_tracin_score(
-        args=args,
-        model = model,
-        weights_paths = paths,
-        train_dataloader = train_dataloader,
-        test_dataloader = test_dataloader,
-        nu_for_each_epoch = 1e-5)
-exist = (score_matrix != 0)
-num = score_matrix.sum(axis = 1)
-den = exist.sum(axis = 1) + 0.01
-avg = num / den
-
-st = np.argsort(avg)
-indexes = st[int(st.shape[0] * 0.8) :]
-noisy_index = [idxs[x] for x in indexes]
-
-if args.model_type in ['gcn', 'gin', 'ggnn', 'hgt']:
-    print('use gnn: ', args.model_type)
-    if args.dataset in ['java250', 'python800']:
-        train_samples, valid_samples, test_samples, token_vocabsize, type_vocabsize = get_spt_dataset(data=args.dataset,
-                                                                                                      mislabeled_rate=args.noise_rate)
-    else:
-        raise NotImplementedError
+    # if args.dataset in ['java250', 'python800']:
+    #     train_samples, valid_samples, test_samples, token_vocabsize, type_vocabsize = get_spt_dataset(data=args.dataset,
+    #                                                                                                   mislabeled_rate=args.noise_rate)
+    # else:
+    #     raise NotImplementedError
     trainset = GraphClassificationDataset(train_samples)
     print('previous example len ', len(trainset.examples))
     for example in trainset.examples:
@@ -189,11 +270,12 @@ if args.model_type in ['gcn', 'gin', 'ggnn', 'hgt']:
     train_dataloader = GraphDataLoader(trainset, batch_size=args.batch_size, shuffle=True)
     valid_dataloader = GraphDataLoader(validset, batch_size=args.batch_size, shuffle=False)
     test_dataloader = GraphDataLoader(testset, batch_size=args.batch_size, shuffle=False)
+
 else:
-    if args.dataset == 'poj':
-        train_samples, valid_samples, test_samples = generate_pojdata(mislabeled_rate=0.2)
-    if args.dataset in ['java250', 'python800']:
-        train_samples, valid_samples, test_samples = read_codenetdata(dataname=args.dataset, mislabeled_rate=0.2)
+    # if args.dataset == 'poj':
+    #     train_samples, valid_samples, test_samples = generate_pojdata(mislabeled_rate=0.2)
+    # if args.dataset in ['java250', 'python800']:
+    #     train_samples, valid_samples, test_samples = read_codenetdata(dataname=args.dataset, mislabeled_rate=0.2)
 
     trainset = ClassificationDataset(tokenizer, args, train_samples)
     print('previous example len ', len(trainset.examples))
@@ -317,8 +399,7 @@ for epoch in range(args.epochs):
         print('----test----')
         test_res = evaluate(model, test_dataloader)
         print(test_res)
-    if epoch % 2 == 0:
-        prefix = "ckpt_" + str(epoch) + '_' + str(valid_acc)
+        prefix = "tracin_" + args.model_type + args.noise_pattern + str(args.noise_rate) + '_' + str(round(test_res['eval_acc'], 4)) + '_' + str(round(precision, 4)) + '_' + str(round(recall, 4))
         out_dir = os.path.join(out_path, prefix)
         if not os.path.exists(out_dir):
             os.makedirs(out_dir)
--- tracin/tracin.py
+++ tracin/tracin.py
@@ -12,12 +12,12 @@ def calculate_tracin_score(
 ):
     LR = nu_for_each_epoch
 
-    score_matrix = np.zeros((len(train_dataloader), len(test_dataloader)))
     indexes = []
     #for train_id, (_, x_train, y_train) in enumerate(train_dataloader):
 
     # test_re = []
-    # model.load_state_dict(torch.load(weights_paths[-1]))
+    print('weight path -1 is ', weights_paths[-1])
+    model.load_state_dict(torch.load(weights_paths[-1]))
     # for test_id, tbatch in enumerate(test_dataloader):
     #     if test_id % 100 == 0:
     #         print('Train:', test_id, '/',  len(test_dataloader))
@@ -59,35 +59,57 @@ def calculate_tracin_score(
     #         score_matrix[train_id][test_id] += grad_sum
     # return score_matrix
 
+    # for train_id, batch in enumerate(train_dataloader):
+    #     x_index = batch[0].to(args.device)
+    #     x_train = batch[1].to(args.device)
+    #     y_train = batch[2].to(args.device)
+    #     indexes.append(x_index.cpu().item())
+    #     for test_id, tbatch in enumerate(test_dataloader):
+    #         x_test = tbatch[1].to(args.device)
+    #         y_test = tbatch[2].to(args.device)
+    #         grad_sum = 0
+    #         if y_test[0] != y_train[0]:
+    #             continue
+    #         for w in weights_paths:
+    #             model.load_state_dict(torch.load(w))  # checkpoint
+    #             model.eval()
+    #             logit = model(x_train)  # pred
+    #             loss = F.cross_entropy(logit, y_train.long(), reduction='mean')
+    #             loss.backward()  # back
+    #             train_grad = torch.cat([param.grad.reshape(-1) for param in model.parameters()])
+    #
+    #             model.load_state_dict(torch.load(w))  # checkpoint
+    #             model.eval()
+    #             logit = model(x_test)  # pred
+    #             loss = F.cross_entropy(logit, y_test.long(), reduction='mean')
+    #             loss.backward()  # back
+    #             test_grad = torch.cat([param.grad.reshape(-1) for param in model.parameters()])
+    #
+    #             grad_sum += LR * np.dot(train_grad.cpu().numpy(), test_grad.cpu().numpy())  # scalar mult, TracIn formula
+    #
+    #         score_matrix[train_id][test_id] += grad_sum
+    score_matrix = np.zeros(len(train_dataloader))
     for train_id, batch in enumerate(train_dataloader):
+        grad_sum = 0
+
+        if train_id % 1000 == 0:
+            print(str(train_id), ' / ', len(train_dataloader))
         x_index = batch[0].to(args.device)
+
         x_train = batch[1].to(args.device)
         y_train = batch[2].to(args.device)
         indexes.append(x_index.cpu().item())
-        for test_id, tbatch in enumerate(test_dataloader):
-            x_test = tbatch[1].to(args.device)
-            y_test = tbatch[2].to(args.device)
-            grad_sum = 0
-            if y_test[0] != y_train[0]:
-                continue
-            for w in weights_paths:
-                model.load_state_dict(torch.load(w))  # checkpoint
-                model.eval()
-                logit = model(x_train)  # pred
-                loss = F.cross_entropy(logit, y_train.long(), reduction='mean')
-                loss.backward()  # back
-                train_grad = torch.cat([param.grad.reshape(-1) for param in model.parameters()])
+        #print('x_index is ', x_index.cpu().item())
 
-                model.load_state_dict(torch.load(w))  # checkpoint
-                model.eval()
-                logit = model(x_test)  # pred
-                loss = F.cross_entropy(logit, y_test.long(), reduction='mean')
-                loss.backward()  # back
-                test_grad = torch.cat([param.grad.reshape(-1) for param in model.parameters()])
+        model.eval()
+        logit = model(x_train)  # pred
+        loss = F.cross_entropy(logit, y_train.long(), reduction='mean')
+        loss.backward()  # back
+        train_grad = torch.cat([param.grad.reshape(-1) for param in model.parameters()])
+        grad_sum += LR * np.dot(train_grad.cpu().numpy(),
+                                train_grad.cpu().numpy())
+        model.zero_grad()
 
-                grad_sum += LR * np.dot(train_grad.cpu().numpy(), test_grad.cpu().numpy())  # scalar mult, TracIn formula
-
-            score_matrix[train_id][test_id] += grad_sum
-
+        score_matrix[x_index] += grad_sum
 
     return score_matrix, indexes
